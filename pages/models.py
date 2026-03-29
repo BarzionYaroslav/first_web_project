063f5a86a21eee3f758c31831179055644ba70ad
BarzionYaroslav <deleted.2020@mail.ru>
2026-03-29 11:34:17 +0300

    Task 5 Ready

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=256,null=False,blank=False)
+    description = models.TextField(null=False,blank=False)
+    price = models.DecimalField(max_digits=16,decimal_places=2,null=False,blank=False)
+    stock = models.PositiveIntegerField(null=False,blank=False)
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.stock}"
