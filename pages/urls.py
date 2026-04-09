2586d2cfd656b8ecc78ef4965a6e1b25d0a9c32f
BarzionYaroslav <deleted.2020@mail.ru>
2026-04-09 18:27:35 +0300

    Task 7 ready

--- pages/urls.py
+++ pages/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", view.index, name="index"),
     path("categories/", view.categories, name="categories"),
     path("books/", view.book_list, name="books"),
+    path("book/<int:id>/", view.book_details, name="book_detail"),
 ]
