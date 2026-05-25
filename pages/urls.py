61e6243678bedfb7e5b8ea8dff65e504d096e897
BarzionYaroslav <deleted.2020@mail.ru>
2026-05-25 12:07:09 +0300

    Task 13 Ready

--- pages/urls.py
+++ pages/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path("", view.index, name="index"),
     path("accounts/register", view.register, name="register"),
     path("categories/", view.categories, name="categories"),
-    path("books/", view.book_list, name="books"),
-    path("book/<int:id>/", view.book_details, name="book_detail"),
+    path("books/", view.BookList.as_view(), name="books"),
+    path("book/<int:id>/", view.BookDetail.as_view(), name="book_detail"),
     path("book/<int:id>/comment", view.create_comment, name="add_comment"),
-    path("book/<int:id>/edit", view.edit_book, name="edit_book"),
+    path("book/<int:id>/edit", view.EditBook.as_view(), name="edit_book"),
+    path("book/<int:id>/delete",view.DeleteBook.as_view(),name="delete_book"),
     path("contact/", view.contact, name="contact"),
-    path("book/create/", view.create_book, name="create_book")
+    path("book/create/", view.CreateBook.as_view(), name="create_book")
 ]
